e04b22f5a79720756a710eee1c0c17a5e1bc64a4
yoniosin <yoni.osin@gmail.com>
2018-11-12 11:10:18 +0200

    d

--- input manipulation.py
+++ input manipulation.py
@@ -2,21 +2,27 @@ import random
 import numpy as np
 import os
 from scipy import io as sio
-
+import re
 
 def dilate(input_mat, out_size):
     dial_vec = random.sample(range(input_mat.shape[1]), out_size)
     dial_vec.sort()
-    return input_mat[:, dial_vec]
+    dilated_mat = input_mat[:, dial_vec]
+    return dilated_mat
 
 
 def DilateAll(folder_name, out_size):
     for file_name in os.listdir(folder_name):
-        file = sio.loadmat(folder_name + '/' + file_name)
-        new_file = dilate(file['mat'], out_size)
-        sio.savemat('new/' + file.__str__(), {'new_file': new_file})
+        if re.search(r".*.mat", file_name):
+            file = sio.loadmat(folder_name + '/' + file_name)
+            file_data = file['image_data']
+            new_file = dilate(file_data, out_size)
+            sio.savemat('new/' + file_name, {'new_file': new_file})
+        elif re.search(r".*.bmpLR" , file_name):
+            file = sio.loadmat(folder_name + '/' + file_name)
+            sio.savemat('new/' + file_name, {'file': file})
 
 
 if __name__ == '__main__':
-    a = np.random.randint(100, size=(5, 5))
-    DilateAll('C:/Users/yonio/OneDrive/School/semester 8/New', 3)
+   # DilateAll('C:/Users/yonio/OneDrive/School/semester 8/New', 3)
+    DilateAll('/home/yonipeleg/PycharmProjects/UltraSound/image', 64)
